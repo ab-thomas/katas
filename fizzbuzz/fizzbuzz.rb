11113742dd9aa57a1a96c778393719fb5dba5dc1
Abraham Thomas <dysruptmedia@gmail.com>
2014-01-15 16:02:47 +0000

    Adding FizzBuzz

---
+++ fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,19 @@
+
+def divisible_by?(n, divider)
+  n % divider == 0
+end
+
+
+def fizzbuzz(n)
+
+  return "FizzBuzz" if divisible_by?(n, 15)
+     
+  return "Fizz" if divisible_by?(n, 3)
+  
+  return "Buzz" if divisible_by?(n, 5)
+  
+  n
+
+end
+
+
---
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -0,0 +1,28 @@
+require ('./fizzbuzz')
+
+describe "FizzBuzz"  do
+
+  it "should be 1 if 1" do 
+    expect(fizzbuzz(1)).to eq(1)
+  end
+  
+  it "3 should be Fizz" do
+    expect(fizzbuzz(3)).to eq("Fizz")
+  end
+
+  it "5 should be buzz" do 
+    expect(fizzbuzz(5)).to eq("Buzz")
+  end 
+
+  it "15 should return FizzBuzz" do
+    expect(fizzbuzz(15)).to eq("FizzBuzz")
+  end
+
+end
+
+
+
+
+
+
+
